611659060898d39c339bb313c3fce3ee30bad02b
lyraxvincent <njongevincent@gmail.com>
2022-08-06 00:27:40 +0300

    add arg parsing and refactor code

--- src/visual_md/generate_dashboard.py
+++ src/visual_md/generate_dashboard.py
@@ -1,29 +1,58 @@
 import os
+import argparse
+import logging
 import sys
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
-try:
-    include_code = sys.argv[3]
-except:
-    include_code = ''
-
-# convert jupyter notebook(ipynb) to markdown(md)
-os.system(f"jupyter nbconvert {input_file} --to markdown")
-
-fname = f"{input_file.split('.')[0]}.md"
-
-line_numbers = []
-img_calls = []
-
-for l_number, l in enumerate(open(fname, "r").readlines(), start=1):
-    if l.startswith("```"):
-        line_numbers.append(l_number)
-    elif l.startswith("![png]"):
-        img_calls.append(l)
+_logger = logging.getLogger(__name__)
 
 
-def get_codeCells(line_numbers):
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(
+        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--help"]``).
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(description="visual-md: jupyter notebooks to visual reports.")
+    parser.add_argument(
+        "--include-code",
+        "--include-code",
+        dest="include_code",
+        action="store_const",
+        const=True,
+    )
+    parser.add_argument(
+        "-i",
+        "--input-file",
+        dest="input",
+        help="provide input file",
+    )
+    parser.add_argument(
+        "-o",
+        "--output-file",
+        dest="output",
+        help="provide output filename",
+    )
+
+    return parser.parse_args(args)
+
+
+def get_codeCells(line_numbers, fname):
     """
 
     :param line_numbers:
@@ -47,10 +76,11 @@ def get_codeCells(line_numbers):
             code_cells.append(code_cell)
 
     os.remove(fname)
+    
     return code_cells
 
 
-def insert_images(code_cells, image_calls, include_code=str(include_code)):
+def insert_images(code_cells, image_calls, include_code: bool):
     """
 
     :param code_cells:
@@ -63,27 +93,49 @@ def insert_images(code_cells, image_calls, include_code=str(include_code)):
 
     document = """<h1 align="center">Plots</h1>\n\n-----\n\n"""
 
-    if include_code == "--include-code":
+    if include_code:
         for code_cell, img_call in zip(code_cells, image_calls):
 
             # centered image
-            img_call_centered = f"""\n<p align="center">\n\t<img src='{img_call.split(']')[-1].strip().strip(')').strip('(')}', alt='plot'/>\n</p>\n"""
+            img_call_centered = f"""\n<p align="center">\n\t<img src='{img_call.split(']')[-1].strip().strip(')').strip('(')}'/>\n</p>\n"""
 
             document += ('```python\n' + code_cell + "\n" + img_call_centered + "\n")
     else:
         for img_call in image_calls:
-            img_call_centered = f"""\n<p align="center">\n\t<img src='{img_call.split(']')[-1].strip().strip(')').strip('(')}', alt='plot'/>\n</p>\n"""
+            img_call_centered = f"""\n<p align="center">\n\t<img src='{img_call.split(']')[-1].strip().strip(')').strip('(')}'/>\n</p>\n"""
             document += ("\n" + img_call_centered + "\n")
 
     return document
 
 
-# Applying functions to md file
-cells = get_codeCells(line_numbers=line_numbers)
-doc = insert_images(cells, image_calls=img_calls, include_code=include_code)
+def main(args):
 
+    args = parse_args(args)
+    setup_logging(logging.INFO)
 
-# export document
-with open(output_file, "w") as out_file:
-    out_file.write(doc)
-    out_file.close()
+    # convert jupyter notebook(ipynb) to markdown(md)
+    input_file = args.input
+    os.system(f"jupyter nbconvert {input_file} --to markdown")
+    fname = f"{'.'.join(input_file.split('.')[:-1])}.md"
+    line_numbers = []
+    img_calls = []
+
+    for l_number, l in enumerate(open(fname, "r").readlines(), start=1):
+        if l.startswith("```"):
+            line_numbers.append(l_number)
+        elif l.startswith("![png]"):
+            img_calls.append(l)
+
+    code_cells = get_codeCells(line_numbers, fname)
+
+    document = insert_images(code_cells, image_calls=img_calls, include_code=args.include_code)
+
+    # export document
+    with open(args.output, "w") as out_file:
+        out_file.write(document)
+        out_file.close()
+
+    _logger.info(f"saved output file as [{args.output}]")
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
